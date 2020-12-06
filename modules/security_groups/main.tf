e5eeecafed363ed127731b493417cc9aeb8e663f
ralimardanov <rafik.alimardanov@gmail.com>
2020-12-06 22:59:15 +0300

    Made changes in Security Group module

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -2,29 +2,34 @@
 data "aws_availability_zones" "available_azs" {}
 
 resource "aws_security_group" "main_sg" {
-  count  = var.sg_value ? 1 : 0 ### use in network module
+  count  = var.sg_value ? 1 : 0 
   name   = "${var.common_tags["ENV"]}-SG"
   vpc_id = var.vpc_id
 
-  ### use it like this or use with aws_security_groups_rules ?
+  lifecycle {
+    create_before_destroy = true
+  } 
 
   dynamic "ingress" {
-    for_each = var.list_of_ports
+    for_each = var.sg_ingress_values
     content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = var.ingress_protocol
-      cidr_blocks = var.cidr_blocks
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.description
     }
+ }
 
-  }
-
-  egress {
-    from_port   = var.egress_from_port
-    to_port     = var.egress_to_port
-    protocol    = var.egress_protocol
-    cidr_blocks = var.egress_cidr_blocks
-  }
+  dynamic "egress" {
+    for_each = var.sg_egress_values
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
+ }
 
   tags = merge(var.common_tags, { Name = "${var.common_tags["Env"]}-SG" })
 }
--- modules/security_groups/variables.tf
+++ modules/security_groups/variables.tf
@@ -7,3 +7,18 @@ variable "vpc_id" {
   description = "VPC ID for SG"
   type        = string
 }
+
+variable "sg_value" {
+  description = "True/False for security group creation"
+  type = boolean
+}
+
+variable "sg_ingress_values" {
+  description = "List of Ingress ports and etc needed for SG"
+  type = map
+}
+
+variable "sg_egress_values" {
+  description = "List of Egress ports and etc needed for SG"
+  type = map
+}
